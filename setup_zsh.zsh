7500bcc22b1852107ea23b8e2733081c5aa39ea0
Ahmed Elbehary <29628969+Osman190@users.noreply.github.com>
2021-05-12 20:02:14 +0200

    edit setup_zsh file

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,7 +5,7 @@ echo "\n<<< Statring ZSH Setup >>>\n"
 # Installtion unnecessry; it's in Brewfile
 
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'>/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/opt/homebrew/bin/zsh'
