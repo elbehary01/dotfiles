3efdd3ef364ae8d390d5c2d063509e54c4a208e8
Ahmed Elbehary <ahmedelbehary0@gmail.com>
2021-06-03 13:23:15 +0200

    write a conditional  to prevent duplication zsh and symlink the /bin/zsh to the sh shell

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -2,10 +2,32 @@
 
 echo "\n<<< Statring ZSH Setup >>>\n"
 
-# Installtion unnecessry; it's in Brewfile
+# Installtion unnecessry; it's in Brewfile.
 
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'>/dev/null
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/opt/hombrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/hombrew/bin/zsh already exists in /etc/shells'
+else 
+#  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'>/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+
+if ["$SHELL" = '/opt/homebrew/bin/zsh']; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+
+if sh --version | grep -q zsh; then
+  echo "/private/var/select/sh already linked to /bin/zsh"
+else
+  echo "Enter superuser (sudo) password to symlink sh to zsh"
+  # Looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh 
+
+  # I'd like for this to work instead.
+  # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh 
+fi
