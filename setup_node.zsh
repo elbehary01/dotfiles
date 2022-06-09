1c94585b662331613eef71344984d6da22f9d651
Ahmed Elbehary <29628969+elbehary01@users.noreply.github.com>
2022-06-09 10:50:09 +0200

    modify node.zsh

--- setup_node.zsh
+++ setup_node.zsh
@@ -2,7 +2,7 @@
 
 echo "\n<<< Statring Node Setup >>>\n"
 
-# Node versions are manged with `nvm`, which is the Brewfile. 
+# Node versions are manged with `nvm`, which is the Brewfile.
 
 if exists node; then
   echo "Node $(node --version) & NPM $(npm --version) already installed"
@@ -14,11 +14,11 @@ fi
 
 
 # Install Global NPM Packages
-npm install --global nodemon
-npm install --global typescript
-npm install --global json-server
-npm install --global http-server
-npm install --global trash-cli
+npm install --location=global nodemon
+npm install --location=global typescript
+npm install --location=global json-server
+npm install --location=global http-server
+npm install --location=global trash-cli
 
 echo "Global NPM Packages Installed:"
-npm list --global --depth=0
+npm list --location=global --depth=0
